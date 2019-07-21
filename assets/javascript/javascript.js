980e0189e14ca2c6de298d38a5f6828ed9185589
Brett Scottini <bscottini1@gmail.com>
2019-07-21 18:22:46 -0400

    button values

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,16 @@
+let ComputerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+let UserScore = "";
+let Wins = "";
+let Losses = "";
+
+
+let ruby = Math.floor(Math.random()*12)+1;
+let diamond = Math.floor(Math.random()*12)+1;
+let emerald = Math.floor(Math.random()*12)+1;
+let topaz = Math.floor(Math.random()*12)+1;
+
+
+console.log(ruby);
+console.log(diamond);
+console.log(emerald);
+console.log(topaz);
