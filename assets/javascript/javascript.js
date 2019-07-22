93050b5c950423483c2b6d4eb618999e33a68b57
Brett Scottini <bscottini1@gmail.com>
2019-07-22 10:37:59 -0400

    links fixed, button images

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,14 +3,20 @@ let UserScore = "";
 let Wins = "";
 let Losses = "";
 
-
+// gems [ruby, diamond, emerald, topaz];
 let ruby = Math.floor(Math.random()*12)+1;
 let diamond = Math.floor(Math.random()*12)+1;
 let emerald = Math.floor(Math.random()*12)+1;
 let topaz = Math.floor(Math.random()*12)+1;
 
 
+
+
+//$("#ComputerScore").append("11");
+
+
 console.log(ruby);
 console.log(diamond);
 console.log(emerald);
 console.log(topaz);
+// console.log (gems [2]);
