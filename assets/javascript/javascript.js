86087505fc6c40f332cb3bc08c858d35823499d0
Brett Scottini <bscottini1@gmail.com>
2019-07-23 09:59:11 -0400

    All functions working

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,30 +1,57 @@
 let ComputerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-let UserScore = ""; //click value + click value
-let Wins = "";
-let Losses = "";
+let UserScore = 0; //click value + click value
+let Wins = 0;
+let Losses = 0;
 
 let ruby = Math.floor(Math.random()*12)+1;
 let diamond = Math.floor(Math.random()*12)+1;
 let emerald = Math.floor(Math.random()*12)+1;
 let topaz = Math.floor(Math.random()*12)+1;
 
-if (UserScore === ComputerScore) {
-    Wins +=1 && alert("You Win!")
-};
+const crystals = {
+    ruby: {
+        value: Math.floor(Math.random()*12)+1
+    },
+    diamond: {
+        value: Math.floor(Math.random()*12)+1
+    },
+    emerald: {
+        value: Math.floor(Math.random()*12)+1
+    },
+    topaz: {
+        value: Math.floor(Math.random()*12)+1
+    },
+}
 
-if (UserScore > ComputerScore) {
-    Losses += 1 && alert("You Lose!")
-};
 
 
 
 $(document).ready(function() {
 
 $("#ComputerScore").append(ComputerScore);
-$("#UserScore").on("click", function() {
-$("#ruby").on("click", function () {
-    alert("You clicked ruby!");
-    });
+$(".crystalButton").on("click", function() {
+    const crystalValue = $(this).val();
+    console.log(crystalValue);
+    const crystalNum = crystals [crystalValue].value;
+    console.log(crystalNum);
+    UserScore+=crystalNum;
+    $("#UserScore").text(UserScore);
+    if (UserScore === ComputerScore) {
+        Wins +=1 ;
+        alert("You Win!");
+        $("#Wins").html(`<p>Wins: ${Wins}</p>`);
+        
+    };
+    
+    if (UserScore > ComputerScore) {
+        Losses += 1;  
+        alert("You Lose!");
+        $("#Losses").html(`<p>Losses: ${Losses}</p>`);
+    };
+    
+//$("#ruby").on("click", function () {
+  //  alert("You clicked ruby!");
+    //});
 });
 
 
