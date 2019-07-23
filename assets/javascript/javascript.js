40fa7147e71d2b6741c739ce7dae04d3125f90bd
Brett Scottini <bscottini1@gmail.com>
2019-07-22 22:01:32 -0400

    Clicks DON'T WORK!

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,5 @@
 let ComputerScore = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-let UserScore = "";
+let UserScore = ""; //click value + click value
 let Wins = "";
 let Losses = "";
 
@@ -8,15 +8,34 @@ let diamond = Math.floor(Math.random()*12)+1;
 let emerald = Math.floor(Math.random()*12)+1;
 let topaz = Math.floor(Math.random()*12)+1;
 
+if (UserScore === ComputerScore) {
+    Wins +=1 && alert("You Win!")
+};
+
+if (UserScore > ComputerScore) {
+    Losses += 1 && alert("You Lose!")
+};
 
 
 
-$(document).ready() 
+$(document).ready(function() {
 
-$("#ComputerScore").apend(ComputerScore);
+$("#ComputerScore").append(ComputerScore);
+$("#UserScore").on("click", function() {
+$("#ruby").on("click", function () {
+    alert("You clicked ruby!");
+    });
+});
+
 
 //console.log(ruby);
 //console.log(diamond);
 //console.log(emerald);
 //console.log(topaz);
-// console.log (gems [2]);
+
+
+
+
+
+
+});
